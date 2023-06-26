7e87be3dccdc4739119b91e8fecdc0917395ad28
Mary Therese Meldionne Caseria <caseria.therese@gmail.com>
2023-06-27 00:06:41 +0800

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="grid h-screen place-items-center text-xl">
+        WELCOME TO RAILWIND
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ test-folder/script.js
@@ -0,0 +1,3 @@
+const isRoot = require('index.html');
+
+console.log(isRoot)
